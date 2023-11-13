1f64358c5fc3786579443ea6297f2afc0a57cd59
Ankiiisharma <myselfankit.2003@gmail.com>
2023-11-13 13:05:44 +0530

    Week 1.1

---
+++ Week1.1.js
@@ -0,0 +1,23 @@
+// var name = 'Ankit Sharma';
+// var age = 20 ;
+
+// var users = ['Ankit', 'Devansh' , 'Amit', 'Aman'];
+// // console.log(users[0]);
+// // console.log(users[1]);
+
+// //Loops
+
+// // for (let i=0; i<users.length;i++){
+// //   console.log(users[i]);
+// // }
+
+var users = [
+    { name: 'Ankit', age: 20 },
+    { name: 'Sharma', age: 18 },
+    { name: 'Singh', age: 19 }
+  ];
+  
+  for (var i = 0; i < users.length; i++) {
+    console.log(users[i].name + ' s age is '  +  users[i].age);
+  }
+  
