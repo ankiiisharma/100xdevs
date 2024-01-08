d1d294ca4358f18c16e678b300488642d1a952d9
Ankiiisharma <myselfankit.2003@gmail.com>
2024-01-08 14:45:17 +0530

    Hospital backend assignment

--- week2.5/hospital.js
+++ week2.5/hospital.js
@@ -1,20 +1,22 @@
 const express = require("express");
+const { userInfo } = require("os");
 const app = express();
+const port = 3000;
 
 var users = [{
-    name : "Ramukaka",
-    kidneys : [{
-        healthy : false
+    name: "Ramukaka",
+    kidneys: [{
+        healthy: false
     }]
 }];
 
-app.get('/', function(req, res){
-    const Ramukakkidneys = users[0].kidneys;3
+app.get('/', function (req, res) {
+    const Ramukakkidneys = users[0].kidneys;
     const nummberOfKidneys = Ramukakkidneys.length;
-    const numberOfHealthyKidneys = 0;
+    let numberOfHealthyKidneys = 0;
 
-    for(let i=0;i<Ramukakkidneys.length;i++){
-        if(Ramukakkidneys[i].healthy){
+    for (let i = 0; i < Ramukakkidneys.length; i++) {
+        if (Ramukakkidneys[i].healthy) {
             numberOfHealthyKidneys += 1;
         }
     }
@@ -27,4 +29,71 @@ app.get('/', function(req, res){
     })
 })
 
-app.listen(3000);   
+
+app.use(express.json());
+
+//post
+
+app.post('/', function (req, res) {
+    const isHealthy = req.body.isHealthy;
+    users[0].kidneys.push({
+        healthy: isHealthy
+    })
+    res.json({
+        msg: "DONE!"
+    })
+})
+
+
+
+//PUT
+
+app.put('/', function (req, res) {
+    for (let i = 0; i < users[0].kidneys.lenght; i++) {
+        users[0].kidney[i].healthy = true;
+    }
+    res.json({
+
+    });
+})
+
+
+//DELETE
+
+function isThereAtleastOneUnhealthyKidney() {
+    let atleastoneUnhealthyKidney = false;
+    for (let i = 0; i < users[0].kidneys.length; i++) {
+        if (!users[0].kidneys[i].healthy) {
+            atleastoneUnhealthyKidney = true;
+        }
+    }
+    return atleastoneUnhealthyKidney;
+}
+
+
+app.delete('/', function (req, res) {
+
+    if (isThereAtleastOneUnhealthyKidney()) {
+        const newKidneys = [];
+        for (let i = 0; i < users[0].kidneys.length; i++) {
+            if (users[0].kidneys[i].healthy) {
+                newKidneys.push({
+                    healthy: true
+                })
+            }
+        }
+        users[0].kidneys = newKidneys;
+        res.json({ msg: "done!" })
+    }
+
+    else {
+        res.sendStatus(411).json({
+            msg:"NO BAD KIDNEYS!!"
+        });
+    }
+})
+
+
+app.listen(port, () => {
+    console.log(`App listening at ${port}`);
+});  
