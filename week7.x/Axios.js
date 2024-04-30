0a0144c0ceb48729cbe9b099927a5fdd5d43bb0e
Ankiiisharma <myselfankit.2003@gmail.com>
2024-04-30 20:02:03 +0530

    axios updated

--- week7.x/Axios.js
+++ week7.x/Axios.js
@@ -1,6 +1,16 @@
 const axios = require("axios");
 
 async function axiosRequest() {
-  const response = await axios.get("https://sum-server.100xdevs.com/todos");
+  const response = await axios.get(
+    "https://sum-server.100xdevs.com/todos",
+    {
+      username: "Ankit",
+    },
+    {
+      headers: {
+        Authorization: "Bearer 123",
+      },
+    }
+  );
   console.log(response.data.todos.length);
 }
