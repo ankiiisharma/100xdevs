ef608fc04bf6c79650771e46dfc51930b9418291
Ankiiisharma <myselfankit.2003@gmail.com>
2024-10-07 22:44:57 +0530

    Week 16 - started

---
+++ week16.1/apps/react-app/src/App.tsx
@@ -0,0 +1,14 @@
+import { Admin } from '@repo/ui/admin'
+import "./App.css"
+
+const App = () => {
+  return (
+   <> 
+    <h1>React App</h1>
+    <p>Hi from React App</p>
+    <Admin />
+   </>
+  )
+}
+
+export default App
---
+++ week16.1/apps/web/app/admin/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react' 
+import {Admin }from '@repo/ui/admin'
+const page = () => (
+    <>
+        hi form Admin
+        <Admin />
+    </>
+)
+
+export default page
